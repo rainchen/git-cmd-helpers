b01bbeb46b9617ff9d5ea0b013f8de3c99cbaa10
RainChen <hirainchen@gmail.com>
2014-08-03 11:14:42 +0800

    add github-send-pull-request: use current branch to send pull request on github

---
+++ github-functions.sh
@@ -0,0 +1,21 @@
+# use current branch to send pull request on github
+# will open url like:
+#   "https://github.com/${github_user}/${github_repo}/compare/${base_branch}...${current_branch}"
+# example for using develop as base branch: $ github-send-pull-request develop
+function github-send-pull-request {
+  default_base_branch="master"
+  base_branch=$1
+  # use default_base_branch
+  if [[ $base_branch == "" ]]; then
+    base_branch=$default_base_branch
+  fi
+
+  github_repo_url=`git ls-remote --get-url`
+  github_user=`echo $github_repo_url|cut -d : -f 2|cut -d / -f 1`
+  github_repo=`echo $github_repo_url|cut -d : -f 2|cut -d / -f 2|cut -d . -f 1`
+  current_branch=`git rev-parse --abbrev-ref HEAD`
+
+  github_pr_url="https://github.com/${github_user}/${github_repo}/compare/${base_branch}...${current_branch}"
+  # echo $github_pr_url
+  open $github_pr_url
+}
