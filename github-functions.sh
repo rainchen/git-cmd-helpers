67d9189a19dae0b3e17bff1492920096ebd8250a
RainChen <hirainchen@gmail.com>
2014-08-03 13:39:16 +0800

    add function github-open: open github page for current repo

--- github-functions.sh
+++ github-functions.sh
@@ -1,3 +1,15 @@
+# open github page for current repo
+function github-open {
+  github_repo_url=`git ls-remote --get-url`
+  github_user=`echo $github_repo_url|cut -d : -f 2|cut -d / -f 1`
+  github_repo=`echo $github_repo_url|cut -d : -f 2|cut -d / -f 2|cut -d . -f 1`
+  current_branch=`git rev-parse --abbrev-ref HEAD`
+
+  github_pr_url="https://github.com/${github_user}/${github_repo}"
+  open $github_pr_url
+}
+
+
 # use current branch to send pull request on github
 # will open url like:
 #   "https://github.com/${github_user}/${github_repo}/compare/${base_branch}...${current_branch}"
